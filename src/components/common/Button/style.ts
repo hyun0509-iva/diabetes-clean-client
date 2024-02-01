37b578326eebf666ca12a6bfedf07b8fc24c0cec
hyun0509-iva <skyblue5030@gmail.com>
2024-02-02 08:38:09 +0900

    style: 버튼 공통 컴포넌트 타입 수정

--- src/components/common/Button/style.ts
+++ src/components/common/Button/style.ts
@@ -7,26 +7,41 @@ export const ButtonInterface = styled.button<{
   bgColor?: string;
   color?: string;
 }>`
+  padding: 5px;
+  margin: 0 0.3rem;
+  position: relative;
+  border: 1px solid #dcdcdc;
+  border-radius: 8px;
+  border: 1px solid #ced4da;
+
+  /* custom css */
   width: ${(props) =>
     (typeof props.posX === "number" ? props.posX + "px" : props.posX) ||
     "86px"};
+
   height: ${(props) =>
     (typeof props.posY === "number" ? props.posY + "px" : props.posY) ||
     "35px"};
+
   font-size: ${(props) =>
     (typeof props.size === "number" ? props.size + "px" : props.size) ||
     "16px"};
-  background-color: ${(props) => props.bgColor || "transparent"};
-  color: ${(props) => props.color || "#000"};
-  border: 1px solid #ced4da;
-  border-radius: 8px;
-  cursor: pointer;
 
-  &:hover {
-    opacity: 0.8;
+  background-color: ${(props) => props.bgColor || "transparent"};
+
+  &:disabled {
+    background-color: rgba(239, 239, 239, 0.3);
   }
-  &:active {
-    transform: translateY(3px);
+
+  &:not(:disabled) {
+    color: ${(props) => props.color || "#000"};
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+    &:active {
+      transform: translateY(3px);
+    }
   }
 
   & + & {
