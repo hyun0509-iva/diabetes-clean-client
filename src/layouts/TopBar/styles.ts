d90c97102a232a6ea5b3d17ceb9c577f9621201c
hyun0509-iva <skyblue5030@gmail.com>
2024-02-13 00:24:01 +0900

    design: TopBar 스타일 변경  : OverWrap될 때, 스크롤 내리면 풀리는 현상 수정

--- src/layouts/TopBar/styles.ts
+++ src/layouts/TopBar/styles.ts
@@ -44,12 +44,11 @@ export const Navbar = styled.div<{ isAuth: boolean }>`
   }
 `;
 export const OverWrap = styled.div`
-  position: absolute;
+  position: fixed;
   left: 0;
   right: 0;
   top: 0;
   bottom: 0;
-  height: 100vh;
   background: rgba(0, 0, 0, 0.5);
   z-index: 10;
 `;
